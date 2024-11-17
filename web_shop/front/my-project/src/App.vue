3b1af33d31def460180d2db027d0e63b6c834f73
Ana Rogalo <rogaloana@gmail.com>
2024-11-17 11:19:12 +0100

    commit

--- web_shop/front/my-project/src/App.vue
+++ web_shop/front/my-project/src/App.vue
@@ -1,17 +1,10 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import ProductView from './components/Proizvod.vue';
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
 </template>
 
 <style scoped>
